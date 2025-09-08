53cb53a72aa2f33d96525a27430715f5c46e87d0
mirai <mirai.dev32@gmail.com>
2025-09-08 20:23:46 +0200

    Increased speed through threading

--- modules/port_scanner.py
+++ modules/port_scanner.py
@@ -1,17 +1,30 @@
 import socket
+import threading
 import os
-from colorama import init, Fore, Style
+from colorama import Fore, Style
+
+def scan_port(target, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.5)
+        s.connect((target, port))
+        print(f"  [{Fore.CYAN}>{Style.RESET_ALL}] Port {port} open")
+        s.close()
+    except:
+        pass
 
 def run(target):
     os.system('cls' if os.name == 'nt' else 'clear')
     print(f"\n  [{Fore.GREEN}>>>{Style.RESET_ALL}] Port scan on: {target}")
     print(f"  [{Fore.YELLOW}>>>{Style.RESET_ALL}] This manipulation can take time because it scans ALL ports.\n")
+
+    threads = []
+
     for port in range(0, 65536):
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(0.5)
-            s.connect((target, port))
-            print(f"  [{Fore.CYAN}>{Style.RESET_ALL}] Port {port} open")
-            s.close()
-        except:
-            pass
+        thread = threading.Thread(target=scan_port, args=(target, port))
+        threads.append(thread)
+        thread.start()
+        
+    for thread in threads:
+        thread.join()
+
